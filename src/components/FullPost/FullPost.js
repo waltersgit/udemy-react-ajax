b41a89cc54335cb69e2df5977c4d23a89186220e
peter <peter@TWM005-MAC.local>
2018-03-27 16:51:24 +0800

    lesson 151

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import './FullPost.css';
+
+class FullPost extends Component {
+    state = {
+        lodedPost: null
+    }
+    //得到父組件的id，且本身render完，觸發，此時的lodedPost還是null,在此function中設定完loadedPost後再重新自己本身組件的生命週期
+    componentDidUpdate(){
+        if(this.props.id){
+            if(!this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id)){
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response => {
+                        this.setState({loadedPost: response.data})
+                    });
+            }
+        }
+    }
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if(this.props.id){
+            post = <p style={{textAlign: 'center'}}>Loading...</p>
+        }
+        if(this.state.loadedPost){
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+            );
+        }
+
+        return post;
+    }
+}
+
+export default FullPost;
