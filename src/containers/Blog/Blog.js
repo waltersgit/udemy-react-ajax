786316bef04feb308168ad238cfc7ff0ec1cec47
peter <peter@TWM005-MAC.local>
2018-03-27 17:53:39 +0800

    lesson 155

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,7 +9,8 @@ import './Blog.css';
 class Blog extends Component {
     state = {
         posts: [],
-        selectedPostId: null
+        selectedPostId: null,
+        error: false
     }
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts')
@@ -22,20 +23,28 @@ class Blog extends Component {
                     }
                 })
                 this.setState({posts: updatePosts});
-                console.log(response);
+            })
+            .catch(error => {
+                this.setState({error: true});
             })
     }
     postSelectedHandler= (id) => {
         this.setState({selectedPostId: id});
     }
     render () {
-        const posts = this.state.posts.map(post => {
-            return <Post
-                key={post.id}
-                title={post.title}
-                author={post.author}
-                clicked={()=>this.postSelectedHandler(post.id)}/>
-        })
+        let posts = <p style={{textAlign: 'center'}}>
+            Something went wrong
+        </p>
+        if(!this.state.error){
+            posts = this.state.posts.map(post => {
+                return <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={()=>this.postSelectedHandler(post.id)}/>
+            })
+        }
+
         return (
             <div>
                 <section className="Posts">
